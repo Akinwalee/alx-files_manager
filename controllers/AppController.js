ae53e70419bc214884971981fef10ab8721178d0
Praise Aribisala <aribisalapraise12@gmail.com>
2024-12-09 23:50:25 +0100

    Task 2

---
+++ controllers/AppController.js
@@ -0,0 +1,16 @@
+import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis.js';
+
+class AppController {
+    static getStatus(request, response) {
+        const redis = redisClient.isAlive();
+        const db = dbClient.isAlive();
+        response.status(200).send({ "redis": redis, "db": db });
+    }
+
+    static getStats(request, response) {
+        const users = dbClient.nbUsers();
+        const files = dbClient.nbFiles();
+        response.status(200).send({ "users": users, "files": files })
+    }
+}
